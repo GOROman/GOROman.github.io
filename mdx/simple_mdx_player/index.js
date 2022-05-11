589c0be403cb11ecbfb448f721a36b9819b95fd3
GOROman <yoshihito.kondoh@gmail.com>
2022-05-11 19:58:16 +0900

    オプション指定ミス

--- mdx/simple_mdx_player/index.js
+++ mdx/simple_mdx_player/index.js
@@ -72,7 +72,7 @@ if (typeof window == 'object') {
     Module['arguments'] = window.location.search.substr(1).trim().split('&');
     // If no args were passed arguments = [''], in which case kill the single empty string.
     if (!Module['arguments'][0]) {
-      Module['arguments'] = ['bos04.mdx','bos14.mdx'];
+      Module['arguments'] = ['bos04.mdx'];
     }
   }
 
